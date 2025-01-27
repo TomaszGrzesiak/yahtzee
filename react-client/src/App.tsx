17c3f9c7bf93ca2d924862300f091f3cfd1e1de2
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-01-27 19:08:18 +0400

    feat: react router setup

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Lobby from "./pages/Lobby";
+import Login from "./pages/Login";
+import Game from "./pages/Game";
+import Pending from "./pages/Pending";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/game/:id" element={<Game />} />
+        <Route path="/pending/:id" element={<Pending />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ react-client/src/pages/Pending.tsx
@@ -0,0 +1,5 @@
+const Pending = () => {
+  return <h1>Pending</h1>;
+};
+
+export default Pending;
