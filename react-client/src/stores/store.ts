f5987e1e197b9f39b7430435e04cb1840bb6bc66
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-01-27 19:13:54 +0400

    feat: redux toolkit store setup

--- react-client/src/stores/store.ts
+++ react-client/src/stores/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import playerReducer from "./playerSlice";
+import ongoingGamesReducer from "./ongoingGamesSlice";
+import pendingGamesReducer from "./pendingGamesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    player: playerReducer,
+    ongoingGames: ongoingGamesReducer,
+    pendingGames: pendingGamesReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
